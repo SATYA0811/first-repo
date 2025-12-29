bc142fc212f4e7d0ea287ad5ff99df2e21466bd2
Satya Sannihith Lingutla <sunny@mac.home>
2025-12-29 15:40:32 -0500

    second Commit

--- hello.py
+++ hello.py
@@ -1,5 +1,4 @@
 print ("Hello, Git!")
-print("Hello, World!")
 print("This is a simple Python script.")
 print("Let's learn Git version control.")
 print("Adding more lines to demonstrate Git functionality.")
